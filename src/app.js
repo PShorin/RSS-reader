31d1b78393e40d9be3389c8727b4dfad6fb4de89
Pavel Shorin <pavel.shorin98@gmail.com>
2023-06-30 17:51:37 +0300

    feat: added output of feeds and posts

--- src/app.js
+++ src/app.js
@@ -3,37 +3,46 @@ import onChange from 'on-change';
 import render from './view.js';
 import i18next from 'i18next';
 import resources from './locales/index.js';
-
-const initialState = {
-  inputValue: '',
-  valid: true,
-  process: {
-    processState: 'filling',
-    error: '',
-  },
-  content: {
-    feeds: [],
-  },
-};
-
-const elements = {
-  form: document.querySelector('.rss-form'),
-  input: document.querySelector('input[id="url-input"]'),
-  button: document.querySelector('button[type="submit"]'),
-  feedback: document.querySelector('.feedback'),
-  feeds: document.querySelector('.feeds'),
-  posts: document.querySelector('.posts'),
-  modal: {
-    modalWindow: document.querySelector('.modal'),
-    title: document.querySelector('.modal-title'),
-    body: document.querySelector('.modal-body'),
-    button: document.querySelector('.full-article'),
-  },
-};
+import parser from './parser.js';
+import uniqueId from 'lodash/uniqueId.js';
+import axios from 'axios';
 
 const validate = (url, urlList) => {
   const schema = string().trim().required().url().notOneOf(urlList);
   return schema.validate(url);
+};
+
+const getAxiosResponse = (url) => {
+  const allOrigins = 'https://allorigins.hexlet.app/get';
+  const newUrl = new URL(allOrigins);
+  newUrl.searchParams.set('url', url);
+  newUrl.searchParams.set('disableCache', 'true');
+  return axios.get(newUrl);
+};
+
+const createPosts = (state, newPosts, feedId) => {
+  const preparedPosts = newPosts.map((post) => ({
+    ...post,
+    feedId,
+    id: uniqueId(),
+  }));
+  state.content.posts = [...state.content.posts, ...preparedPosts];
+};
+
+const getNewPosts = (state) => {
+  return state.content.feeds.map(({ link, feedId }) =>
+    getAxiosResponse(link).then((response) => {
+      const { posts } = parser(response.data.contents);
+      const addedPostsLinks = state.content.posts.map((post) => post.link);
+      const newPosts = posts.filter(
+        (post) => !addedPostsLinks.includes(post.link)
+      );
+      if (newPosts.length > 0) {
+        createPosts(state, newPosts, feedId);
+      }
+      return Promise.resolve();
+    })
+  );
 };
 
 export default () => {
@@ -45,10 +54,40 @@ export default () => {
       resources,
     })
     .then(() => {
+      const initialState = {
+        inputValue: '',
+        valid: true,
+        process: {
+          processState: 'filling',
+          error: '',
+        },
+        content: {
+          posts: [],
+          feeds: [],
+        },
+      };
+
+      const elements = {
+        form: document.querySelector('.rss-form'),
+        input: document.querySelector('input[id="url-input"]'),
+        button: document.querySelector('button[type="submit"]'),
+        feedback: document.querySelector('.feedback'),
+        feeds: document.querySelector('.feeds'),
+        posts: document.querySelector('.posts'),
+        modal: {
+          modalWindow: document.querySelector('.modal'),
+          title: document.querySelector('.modal-title'),
+          body: document.querySelector('.modal-body'),
+          button: document.querySelector('.full-article'),
+        },
+      };
+
       const watchedState = onChange(
         initialState,
         render(elements, initialState, i18nInstance)
       );
+
+      getNewPosts(watchedState);
 
       setLocale({
         mixed: {
@@ -75,18 +114,29 @@ export default () => {
           .then(() => {
             watchedState.valid = true;
             watchedState.process.processState = 'sending';
-            console.log('sending');
+            return getAxiosResponse(watchedState.inputValue);
           })
-          .then(() => {
-            watchedState.content.feeds.push({ link: watchedState.inputValue });
+          .then((response) => {
+            const data = response.data.contents;
+            const { feed, posts } = parser(data, i18nInstance, elements);
+            console.log('parser');
+            const feedId = uniqueId();
+
+            watchedState.content.feeds.push({
+              ...feed,
+              feedId,
+              link: watchedState.inputValue,
+            });
+            createPosts(watchedState, posts, feedId);
+
             watchedState.process.processState = 'finished';
             console.log('finished');
           })
           .catch((error) => {
             watchedState.valid = false;
+            console.log(error.message);
             watchedState.process.error = error.message ?? 'defaultError';
             watchedState.process.processState = 'error';
-            console.log('error');
           });
       });
     });
--- src/view.js
+++ src/view.js
@@ -6,6 +6,9 @@ const handlerFinishWithError = (elements, error, i18nInstance) => {
   );
 
   elements.input.classList.add('is-invalid');
+
+  elements.button.disabled = false;
+  elements.input.disabled = false;
 };
 
 const handlerSuccessFinish = (elements, i18nInstance) => {
@@ -14,8 +17,112 @@ const handlerSuccessFinish = (elements, i18nInstance) => {
   elements.feedback.textContent = i18nInstance.t('sucсess');
 
   elements.input.classList.remove('is-invalid');
+  elements.button.removeAttribute('disabled');
+  elements.input.removeAttribute('readonly');
+
   elements.input.focus();
   elements.form.reset();
+};
+
+const renderPosts = (state, div, i18nInstance) => {
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+
+  state.content.posts.forEach((post) => {
+    const { title, link, id } = post;
+
+    const li = document.createElement('li');
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'justify-content-between',
+      'align-items-start',
+      'border-0',
+      'border-end-0'
+    );
+
+    const setAttributes = (el, attrs) => {
+      Object.entries(attrs).forEach((element) => {
+        const [key, value] = element;
+        el.setAttribute(key, value);
+      });
+    };
+
+    const a = document.createElement('a');
+    setAttributes(a, {
+      href: link,
+      'data-id': id,
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    });
+    a.textContent = title;
+
+    const button = document.createElement('button');
+    setAttributes(button, {
+      type: 'button',
+      'data-id': id,
+      'data-bs-toggle': 'modal',
+      'data-bs-target': '#modal',
+    });
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.textContent = i18nInstance.t('button');
+
+    li.append(a, button);
+    ul.append(li);
+  });
+
+  div.append(ul);
+};
+
+const renderFeeds = (state, div) => {
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+
+  state.content.feeds.forEach((feed) => {
+    const { title, description } = feed;
+
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('h6', 'm-0');
+    h3.textContent = title;
+
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.textContent = description;
+
+    li.append(h3, p);
+    ul.append(li);
+  });
+
+  div.append(ul);
+};
+
+const createContainer = (type, elements, state, i18nInstance) => {
+  elements[type].textContent = '';
+
+  const divCard = document.createElement('div');
+  divCard.classList.add('card', 'border-0');
+
+  const divCardBody = document.createElement('div');
+  divCardBody.classList.add('card-body');
+
+  const divCardBodyTitle = document.createElement('h2');
+  divCardBodyTitle.classList.add('card-title', 'h4');
+  divCardBodyTitle.textContent = i18nInstance.t(type);
+
+  divCardBody.append(divCardBodyTitle);
+  divCard.append(divCardBody);
+  elements[type].append(divCard);
+
+  if (type === 'posts') {
+    renderPosts(state, divCard, i18nInstance);
+  }
+
+  if (type === 'feeds') {
+    renderFeeds(state, divCard);
+  }
 };
 
 const handlerProcessState = (elements, state, value, i18nInstance) => {
@@ -29,6 +136,8 @@ const handlerProcessState = (elements, state, value, i18nInstance) => {
       handlerFinishWithError(elements, state.process.error, i18nInstance);
       break;
     case 'sending':
+      elements.button.disabled = true;
+      elements.input.readOnly = true;
       break;
     default:
       throw new Error(`Unknown process state: ${value}`);
@@ -41,6 +150,14 @@ export default (elements, state, i18nInstance) => (path, value) => {
   switch (path) {
     case 'process.processState':
       handlerProcessState(elements, state, value, i18nInstance);
+      break;
+
+    case 'content.posts':
+      createContainer('posts', elements, state, i18nInstance);
+      break;
+
+    case 'content.feeds':
+      createContainer('feeds', elements, state, i18nInstance);
       break;
 
     default:
